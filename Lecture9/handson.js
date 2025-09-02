b9daa657994ffddbb04ba3265c2e9ab2df4f93fe
Sarvadnya <mitanshurcpit@gmail.com>
2025-09-02 11:36:49 +0530

    31-08-2025

---
+++ Lecture9/handson.js
@@ -0,0 +1,68 @@
+//Strings Handson file 
+//str = "Hello Mitanshu, Welcome to Acciojob for a great future ahead";
+// console.log({ str })    //Print Hello Mayuresh
+
+// console.log(str.includes("a"));    //Returns true
+// console.log(str.includes("q"))     //Returns False
+
+// console.log(str.split(" "));
+
+// console.log(str.slice(3));    //Prints string from index 3 to n
+
+//console.log(str.replace("m", "W"))      //replace the first value of m with W
+
+// console.log(str.replaceAll("m", "W"))    //replace all the values of m with W
+
+str = "     Hello, This is Mitanshu from Shirpur, Maharashtra     "
+
+// console.log(str.trim());
+
+// console.log(str.substring(5,12))
+
+// console.log(str.substring(-5))
+
+// console.log(str.slice(-10))
+
+// console.log(str.slice(10,15))
+
+// console.log(str.slice(10))
+
+// console.log(str.split(" "))
+
+// console.log(str.split(","))
+
+// console.log(str.toUpperCase())
+
+// console.log(str.toLowerCase())
+
+//Objects using spread operator 
+// const string = "Vaibhav";
+// const number = 66;
+// const bool = true;
+// const obj = {
+//     name: "Vaibhav"
+// }
+// const newObj = {
+//     ...obj,
+//     string,
+//     bool
+// }
+// console.log({ newObj })
+
+
+// const object = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// const object2 = {
+//     b: 5,
+//     c: 6,
+//     d: 7
+// }
+// const merged = { ...object, ...object2 }
+// console.log({ merged })
+
+// const arr = [10, 20, 30, 40, 50, 60];
+// const arr2 = { ...arr };
+// console.log({ arr2 });
